719e14c6e10e2d24f063c3b2c01a2e97536b5f96
Aum Pauskar <aumpauskar111@gmail.com>
2022-03-25 14:29:30 +0530

    Bug fixes

--- main.c
+++ main.c
@@ -15,40 +15,41 @@ int main() {
 		int col = 0;
 
 		while (fgets(buffer, 1024, fp)) {
-            col = 0;
-            row++;
+			col = 0;
+			row++;
 			if (row == 1) {
-                continue;
+				continue;
 			}
+			char* value = strtok(buffer, ", ");
+			while (value) {
+				// column 1
+				if (col == 0) {
+					printf("Last name : ");
+				}
+				// col 2
+				if (col == 1) {
+					printf("\tFirst name : ");
+				}
+				// col 3
+				if (col == 2) {
+					printf("\tGender : ");
+				}
+				// column 4
+				if (col == 3) {
+					printf("\tAge : ");
+				}
+				// column 5
+				if (col == 4) {
+					printf("\tPlace of birth: ");
+				}
+
+				printf("%s", value);
+				value = strtok(NULL, ", ");
+				col++;
+			}
+
+			printf("\n");
 		}
-		char* value = strtok(buffer, ", ");
-		while (value) {
-            // column 1
-			if (col == 0) {
-				printf("Last name :");
-			}
-
-			// col 2
-			if (col == 1) {
-				printf("\tFirst name :");
-			}
-
-			// col 3
-			if (col == 2) {
-				printf("\tGender :");
-			}
-
-			// column 4
-			if (col == 3) {
-				printf("\tPlace of birth :");
-			}
-
-			printf("%s", value);
-			value = strtok(NULL, ", ");
-			col++;
-		}
-
-		printf("\n");
 	}
 
 	// Close the file
