795abc2d3ead5f07c5083770b8bbaf45ac37e21f
Aum Pauskar <aumpauskar111@gmail.com>
2022-03-25 14:11:31 +0530

    Basic logic added

--- main.c
+++ main.c
@@ -1,9 +1,58 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 
 int main() {
 	FILE *fp = fopen("data.csv", "r");
-	fgetc(fp);
-	ftell(fp);
-	return 0;
 
+	if (!fp) {
+		printf("Unable to open file\n");
+	} else {
+		char buffer[1024];
+
+		int row = 0;
+		int col = 0;
+
+		while (fgets(buffer, 1024, fp)) {
+            col = 0;
+            row++;
+			if (row == 1) {
+                continue;
+			}
+		}
+		char* value = strtok(buffer, ", ");
+		while (value) {
+            // column 1
+			if (col == 0) {
+				printf("Last name :");
+			}
+
+			// col 2
+			if (col == 1) {
+				printf("\tFirst name :");
+			}
+
+			// col 3
+			if (col == 2) {
+				printf("\tGender :");
+			}
+
+			// column 4
+			if (col == 3) {
+				printf("\tPlace of birth :");
+			}
+
+			printf("%s", value);
+			value = strtok(NULL, ", ");
+			col++;
+		}
+
+		printf("\n");
+	}
+
+	// Close the file
+	fclose(fp);
+	return 0;
 }
+    
