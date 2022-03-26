dc508b924d38a7b67503cf643b27e4ce5bc039db
Aum Pauskar <aumpauskar111@gmail.com>
2022-03-26 20:54:40 +0530

    Structs added

--- function.c
+++ function.c
@@ -1,6 +1,67 @@
 #include <string.h>
 #include <stdlib.h>
+#include <stdio.h>
 
-void fetch(char arg[]) {
-	
+int fetch() {
+	struct data {
+		char lname[64];
+		char fname[64];
+		char gender[64];
+		char age[64];
+		char pob[64];
+	} d[64];
+
+	int row = 0, col = 0;
+	int count = 0;
+	char buffer[1024];
+
+	FILE *fp = fopen("data.csv", "r");
+	if (!fp) {
+		printf("Unable to open file\n");
+	} else {
+		printf("Yea\n");
+	}
+
+	char search[64];
+	printf("Enter the search term: ");
+	scanf("%s", search);
+
+	while (fgets(buffer, 1024, fp)) {
+		col = 0;
+		row++;
+		if (row == 1) {
+			continue;
+		}
+
+		char* value = strtok(buffer, ", ");
+
+		while (value) {
+
+			value = strtok(NULL, ", ");
+
+			// column 1
+			if (col == 0) {
+				strcpy(d[count].lname, value);
+			}
+			// col 2
+			if (col == 1) {
+				printf(d[count].fname, value);
+			}
+			// col 3
+			if (col == 2) {
+				printf(d[count].gender, value);
+			}
+			// column 4
+			if (col == 3) {
+				printf(d[count].age, value);
+			}
+			// column 5
+			if (col == 4) {
+				printf(d[count].pob, value);
+			}
+
+			printf("%s", value);
+			col++;
+		}
+	}
 }
--- main.c
+++ main.c
@@ -1,1 +1,9 @@
 #include <stdio.h>
+#include "function.c"
+
+int main() {
+	char input[64];
+	printf("Enter name: ");
+	scanf("%s", input);
+	fetch(input);
+}
